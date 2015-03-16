6815b9a77c5420b0e2b323383251125b2c6270e7
Maxwell Gurewitz <maxthegeek1@gmail.com>
2015-03-16 14:47:18 -0700

    basic socket implementation

--- lib/client/index.js.jsx
+++ lib/client/index.js.jsx
@@ -1,12 +1,44 @@
 window.$ = require('jquery');
 var React = require('react');
+var hl = require('highland');
+var io = require('socket.io-client');
 
-var Chat = React.createClass({
+var ChatRoom = React.createClass({
+  getInitialState: function () {
+    return { messages: [] }
+  },
+  componentDidMount: function () {
+    var socket = io();
+    var self = this;
+
+    hl('click', $('#js-send-message'))
+      .map(function () {
+        return $('#js-message').val();
+      })
+      .each(function (msg) {
+        socket.emit('msg', msg);
+        return $('#js-message').val('');
+      });
+
+    hl('msg', socket)
+      .each(function (msg) {
+        self.setState({ messages: self.state.messages.concat([msg]) });
+      });
+  },
   render: function () {
+
+    var messages = this.state.messages.map(function (msg) {
+      return (<p> { msg } </p>);
+    });
+
     return (
-      <div className="chat">
+      <div className='chat-room'>
+        { messages }
+        <input type='text' id='js-message'></input>
+        <button id='js-send-message'> Send </button>
       </div>
     );
   }
 });
-React.render(<Chat />, $('#content')[0])
+
+React.render(<ChatRoom />, $('#content')[0])
--- scripts/server.js
+++ scripts/server.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+var socketIo = require('socket.io');
 var app = require('../lib/app');
+var sockets = require('../lib/app/sockets');
 
 var server = app.listen(process.env.PORT || 3000, function(err) {
-    if (err) { console.log(err) }
+  if (err) { console.log(err) }
+
+  var io = socketIo(server);
+  io.on('connection', sockets(io));
 });
+
