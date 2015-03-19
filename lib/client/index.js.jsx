ead73acda497ef090171fa56eb34123eafeb17c7
Maxwell Gurewitz <maxthegeek1@gmail.com>
2015-03-19 10:08:01 -0700

    animations

--- lib/client/index.js.jsx
+++ lib/client/index.js.jsx
@@ -1,21 +1,15 @@
 window.$ = require('jquery');
 window.JQuery = require('jquery');
 window._ = require('lodash');
+window.hl = require('highland');
 
 var React = require('react');
-var hl = require('highland');
 var io = require('socket.io-client');
 
-var badWords = ['fook', 'shot']
+var curseWords = ['fak', 'shet']
   .map(function (word) {
     return new RegExp(word, 'g');
   });
-
-function symbolizeBadWords (msg) {
-  return badWords.reduce(function (filtered, rgx) {
-    return filtered.replace(rgx, '*$#!%^*');
-  }, msg);
-}
 
 var ChatRoom = React.createClass({
 
@@ -27,20 +21,9 @@ var ChatRoom = React.createClass({
     var socket = io();
     var self = this;
 
-    var enterPresses = hl('keypress', $('#js-message'))
-      .filter(function (e) {
-        return e.keyCode === 13;
-      });
-
-    var clicks = hl('click', $('#js-send-message'));
-
-    hl([enterPresses, clicks])
-      .merge()
+    hl('click', $('#js-send-message'))
       .map(function () {
-        return $('#js-message').val();
-      })
-      .filter(function (msg) {
-        return !_.isEmpty(_.trim(msg));
+        return { message: $('#js-message').val(), isMessage: true };
       })
       .each(function (msg) {
         socket.emit('msg', msg);
@@ -48,21 +31,29 @@ var ChatRoom = React.createClass({
       });
 
     hl('msg', socket)
-      .map(symbolizeBadWords)
       .each(function (msg) {
         self.setState({ messages: self.state.messages.concat([msg]) });
       });
   },
 
   componentDidUpdate: function() {
-    var node = $('#js-message-list');
-    node.scrollTop(node[0].scrollHeight);
+    scrollBottom();
   },
 
   render: function () {
+
     var messages = this.state.messages.map(function (msg, i) {
-      var className = 'message' + (i % 2 === 0 ? ' dark' : ' light');
-      return (<div className = { className } > { msg } </div>);
+      var className = 'message js-message' + (i % 2 === 0 ? ' dark' : ' light');
+
+      if (msg.isImage) {
+        return (
+          <div className = { className } >
+            <img src = { msg.src } onLoad = { scrollBottom }> </img>
+          </div>
+        );
+      } else if (msg.isMessage) {
+        return (<div className = { className } > { msg } </div>);
+      }
     });
 
     return (
@@ -81,3 +72,80 @@ var ChatRoom = React.createClass({
 });
 
 React.render(<ChatRoom />, $('#js-content')[0]);
+
+// var enterPresses = hl('keypress', $('#js-message'))
+//   .filter(function (e) {
+//     return e.keyCode === 13;
+//   });
+
+// var clicks = hl('click', $('#js-send-message'));
+
+// hl([enterPresses, clicks])
+//   .merge()
+
+// function isNotBlank (msg) {
+//   return !_.isEmpty(_.trim(msg.message));
+// }
+
+// function symbolizeCurseWords (msg) {
+//   var message = curseWords.reduce(function (filtered, rgx) {
+//     return filtered.replace(rgx, '*$#!%^*');
+//   }, msg.message);
+
+//   msg.message = message;
+//   return msg;
+// } 
+
+// function scrollBottom () {
+//   var list = $('#js-message-list');
+//   list.scrollTop(list[0].scrollHeight);
+// }
+          
+// function animateMe (stream) {
+
+//     var streamWithQueries = stream.map(function (msg) {
+//       var match = msg.message.match(/animate( me)? (.*)/i);
+//       if(!match) { return msg; }
+//       return [msg, { query: match[2], isQuery: true }];
+//     }).flatten();
+
+//     var imageStream = streamWithQueries
+//       .where({ isQuery: true })
+//       .map(function (queryObj) {
+//         return googleImages(queryObj.query);
+//       })
+//       .series()
+//       .map(function (results) {
+//         var result = _.sample(results);
+//         return { src: result.src, isImage: true };
+//       });
+      
+//     var messageStream = streamWithQueries
+//       .fork()
+//       .filter(function (msg) { return !msg.isQuery })
+
+//     return hl([messageStream, imageStream]).merge();
+// }
+
+// function googleImages (query) {
+
+//   var params = {
+//     v: '1.0',
+//     q: query,
+//     start: '1',
+//     safe: 'active',
+//     imgtype: 'animated',
+//     rsz: '8' 
+//   };
+
+//   return hl($.ajax({
+//     url: 'http://ajax.googleapis.com/ajax/services/search/images?' + $.param(params),
+//     dataType: 'jsonp',
+//     method: 'GET'
+//   })).map(function (res) {
+//     var googleResults = res.responseData.results;
+//     return _.map(googleResults, function (gRes) {
+//       return { src: gRes.unescapedUrl };
+//     });
+//   });
+// }
